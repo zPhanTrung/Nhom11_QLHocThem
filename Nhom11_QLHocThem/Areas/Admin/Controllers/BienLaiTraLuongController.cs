3056bdda6b150834ebc3c97c096a76fa948ca930
Chungmanh <77792732+Chungmanh@users.noreply.github.com>
2021-12-01 20:28:21 +0700

    giaovien3
    giáo vien3

---
+++ Nhom11_QLHocThem/Areas/Admin/Controllers/BienLaiTraLuongController.cs
@@ -0,0 +1,93 @@
+﻿using Nhom11_QLHocThem.Areas.Admin.Dao;
+using Nhom11_QLHocThem.Areas.Admin.Model;
+using Nhom11_QLHocThem.Areas.Admin.Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nhom11_QLHocThem.Areas.Admin.Controllers
+{
+    public class BienLaiTraLuongController : Controller
+    {
+        // GET: Admin/BienLaiTraLuong
+        public ActionResult Index()
+        {
+            List<BLTraLuong_GiaoVien> ds_bienlaitraluong = BienLaiTraLuongDao.GetAllBienLaiTraLuong();
+            return View(ds_bienlaitraluong);
+        }
+
+        // GET: Admin/BienLaiTraLuong/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/BienLaiTraLuong/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/BienLaiTraLuong/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                BienLaiTraLuongDao.InsertBienLaiTraLuong(collection);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/BienLaiTraLuong/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/BienLaiTraLuong/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/BienLaiTraLuong/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/BienLaiTraLuong/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Nhom11_QLHocThem/Areas/Admin/Dao/BienLaiTraLuongDao.cs
@@ -0,0 +1,77 @@
+﻿using Nhom11_QLHocThem.Areas.Admin.Model;
+using Nhom11_QLHocThem.Areas.Admin.Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nhom11_QLHocThem.Areas.Admin.Dao
+{
+    public class BienLaiTraLuongDao
+    {
+        private static SqlConnection connection;
+
+        public static List<BLTraLuong_GiaoVien> GetAllBienLaiTraLuong()
+        {
+            connection = Connection.GetConnection();
+            string queryString = "SELECT MaBLTraLuong, TenGiaoVien, NgayTra, " +
+                "TraChoThangNam, TrangThai, TongLuong FROM BienLaiTraLuong bl, GiaoVien gv " +
+                "WHERE bl.MaGiaoVien = gv.MaGiaoVien";
+            List<BLTraLuong_GiaoVien> bienlaitraluongs = new List<BLTraLuong_GiaoVien>();
+            SqlCommand command = new SqlCommand(queryString, connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Type type = typeof(BLTraLuong_GiaoVien);
+                    BLTraLuong_GiaoVien obj = (BLTraLuong_GiaoVien)Activator.CreateInstance(type);
+                    PropertyInfo[] properties = obj.GetType().GetProperties();
+
+                    foreach (PropertyInfo property in properties)
+                    {
+                        try
+                        {
+                            var value = reader[property.Name];
+                            if (value != null)
+                                property.SetValue(obj, Convert.ChangeType(value.ToString(), property.PropertyType));
+
+                        }
+                        catch { }
+                    }
+                    bienlaitraluongs.Add(obj);
+                }
+                reader.Close();
+            }
+            catch
+            { }
+
+            return bienlaitraluongs;
+        }
+        public static bool InsertBienLaiTraLuong(FormCollection collection)
+        {
+            connection = Connection.GetConnection();
+
+            connection.Open();
+            SqlCommand command = new SqlCommand("P_AddBLTraLuong", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@NgayTra", collection["ngaytra"]);
+            command.Parameters.AddWithValue("@Trachothangnam", collection["trachothangnam"].ToString());
+            command.Parameters.AddWithValue("@TrangThai", 0);
+            command.Parameters.AddWithValue("@MaGiaoVien", collection["magiaovien"]);
+
+            int rs = command.ExecuteNonQuery();
+            connection.Close();
+            if (rs > 0)
+                return false;
+            return true;
+        }
+    }
+}
