825cd40d85cb2f7c45b304edb15b7a0243faaed4
nghiatran110700 <71220569+nghiatran110700@users.noreply.github.com>
2021-12-01 20:39:58 +0700

    model đăng ký

---
+++ Nhom11_QLHocThem/Areas/Admin/Controllers/DangKyController.cs
@@ -0,0 +1,45 @@
+﻿using Nhom11_QLHocThem.Areas.Admin.Dao;
+using Nhom11_QLHocThem.Areas.Admin.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nhom11_QLHocThem.Areas.Admin.Controllers
+{
+    public class DangKyController : Controller
+    {
+       public DangKyDAO getDK()
+        {
+            DangKyDAO dangKy = Session["DangKy"] as DangKyDAO;
+            if(dangKy == null || Session["DangKy"] == null)
+            {
+                dangKy = new DangKyDAO();
+                Session["DangKy"] = dangKy;
+            }
+            return dangKy;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List_Class()
+        {
+            List<LopHocDTO> dao = LopHocDAO.List_Class_DTO();
+            return View(dao);
+        }
+        public ActionResult List_DK()
+        {
+            return View(getDK().getList());
+        }
+        public ActionResult add_class(string id)
+        {
+            getDK().add_list(id);
+            return RedirectToAction("Index","DangKy");
+        }
+
+    }
+}
---
+++ Nhom11_QLHocThem/Areas/Admin/Model/DTO/LopHocDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nhom11_QLHocThem.Areas.Admin.DTO
+{
+    public class LopHocDTO
+    {
+        public string MaLopHoc { get; set; }
+        public string TenLopHoc { get; set; }
+        public decimal HocPhi1Buoi { get; set; }
+        public int SoLuongHocSinh { get; set; }
+        public int HocPhiMotBuoi { get; set; }
+        public string MaGiaoVien { get; set; }
+        public string MaMonHoc { get; set; }
+        public string MaNienKhoa { get; set; }
+        public DateTime NgayBatDau { get; set; }
+        public DateTime NgayKetThuc { get; set; }
+        public DateTime NgayBatDauDangKy { get; set; }
+        public DateTime NgayKetThucDangKy { get; set; }
+        public string TenMonHoc { get; set; }
+        public int SoLuong { get; set; }
+        public string TenNienKhoa { get; set; }
+        public string NamHoc { get; set; }
+    }
+}
